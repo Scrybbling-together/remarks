c6415f3888e74f38cc7f9d3d467634c2fb6728c3
Laura Brekelmans <mail@laura.fm>
2025-01-12 19:06:15 +0100

    Small cleanups

--- test_initial.py
+++ test_initial.py
@@ -146,47 +146,47 @@ r"""
 |_|    |_____/|_|     
 """
 
-# @with_remarks(on_computable_numbers['.rmn_source'])
-# def test_v5_document():
-#     on_computable_numbers_rmc = fitz.open(f"tests/out/{on_computable_numbers['notebook_name']} _remarks.pdf")
-#     assert is_valid_pdf(on_computable_numbers_rmc)
-#     assert on_computable_numbers_rmc.page_count == on_computable_numbers["export_properties"]["merged_pages"]
-#
-#     # There should be a warning, since v5 is not yet supported by the rmc-renderer
-#     assert_scrybble_warning_appears_on_page(on_computable_numbers_rmc, on_computable_numbers['.rm_files'][0]['output_document_position'])
-#     assert_scrybble_warning_appears_on_page(on_computable_numbers_rmc, on_computable_numbers['.rm_files'][1]['output_document_position'])
-#     assert_scrybble_warning_appears_on_page(on_computable_numbers_rmc, on_computable_numbers['.rm_files'][2]['output_document_position'])
-#
-# @with_remarks(black_and_white_document['.rmn_source'])
-# def test_renders_notebook_with_single_v6_page_properly():
-#     black_and_white_rmc = fitz.open(f"tests/out/{black_and_white_document['notebook_name']} _remarks.pdf")
-#     assert is_valid_pdf(black_and_white_rmc)
-#     assert black_and_white_rmc.page_count == black_and_white_document["export_properties"]["merged_pages"]
-#
-#     assert_page_renders_without_warnings(black_and_white_rmc, black_and_white_document['.rm_files'][0]['output_document_position'])
-#
-# @with_remarks(colored_real_document[".rmn_source"])
-# def test_renders_notebook_with_rmpp_v6_colors_properly():
-#     colored_document = fitz.open(f"tests/out/{colored_real_document['notebook_name']} _remarks.pdf")
-#     assert is_valid_pdf(colored_document)
-#     assert colored_document.page_count == colored_real_document["export_properties"]["merged_pages"]
-#
-#     assert_page_renders_without_warnings(colored_document, colored_real_document['.rm_files'][0]['output_document_position'])
-#     assert_page_renders_without_warnings(colored_document, colored_real_document['.rm_files'][0]['output_document_position'])
-#     assert_page_renders_without_warnings(colored_document, colored_real_document['.rm_files'][0]['output_document_position'])
-#     assert_page_renders_without_warnings(colored_document, colored_real_document['.rm_files'][0]['output_document_position'])
-#
-#
-# @with_remarks(gosper_notebook['.rmn_source'])
-# def test_pdf_output():
-#     gosper_rmc = fitz.open(f"tests/out/{gosper_notebook['notebook_name']} _remarks.pdf")
-#     assert is_valid_pdf(gosper_rmc)
-#     assert gosper_rmc.page_count == gosper_notebook["export_properties"]["merged_pages"]
-#
-#     # There should be a warning, since v3 is not yet supported by the rmc-renderer
-#     assert_scrybble_warning_appears_on_page(gosper_rmc, gosper_notebook['.rm_files'][0]['output_document_position'])
-#     assert_scrybble_warning_appears_on_page(gosper_rmc, gosper_notebook['.rm_files'][1]['output_document_position'])
-#     assert_scrybble_warning_appears_on_page(gosper_rmc, gosper_notebook['.rm_files'][2]['output_document_position'])
+@with_remarks(on_computable_numbers['.rmn_source'])
+def test_v5_document():
+    on_computable_numbers_rmc = fitz.open(f"tests/out/{on_computable_numbers['notebook_name']} _remarks.pdf")
+    assert is_valid_pdf(on_computable_numbers_rmc)
+    assert on_computable_numbers_rmc.page_count == on_computable_numbers["export_properties"]["merged_pages"]
+
+    # There should be a warning, since v5 is not yet supported by the rmc-renderer
+    assert_scrybble_warning_appears_on_page(on_computable_numbers_rmc, on_computable_numbers['.rm_files'][0]['output_document_position'])
+    assert_scrybble_warning_appears_on_page(on_computable_numbers_rmc, on_computable_numbers['.rm_files'][1]['output_document_position'])
+    assert_scrybble_warning_appears_on_page(on_computable_numbers_rmc, on_computable_numbers['.rm_files'][2]['output_document_position'])
+
+@with_remarks(black_and_white_document['.rmn_source'])
+def test_renders_notebook_with_single_v6_page_properly():
+    black_and_white_rmc = fitz.open(f"tests/out/{black_and_white_document['notebook_name']} _remarks.pdf")
+    assert is_valid_pdf(black_and_white_rmc)
+    assert black_and_white_rmc.page_count == black_and_white_document["export_properties"]["merged_pages"]
+
+    assert_page_renders_without_warnings(black_and_white_rmc, black_and_white_document['.rm_files'][0]['output_document_position'])
+
+@with_remarks(colored_real_document[".rmn_source"])
+def test_renders_notebook_with_rmpp_v6_colors_properly():
+    colored_document = fitz.open(f"tests/out/{colored_real_document['notebook_name']} _remarks.pdf")
+    assert is_valid_pdf(colored_document)
+    assert colored_document.page_count == colored_real_document["export_properties"]["merged_pages"]
+
+    assert_page_renders_without_warnings(colored_document, colored_real_document['.rm_files'][0]['output_document_position'])
+    assert_page_renders_without_warnings(colored_document, colored_real_document['.rm_files'][0]['output_document_position'])
+    assert_page_renders_without_warnings(colored_document, colored_real_document['.rm_files'][0]['output_document_position'])
+    assert_page_renders_without_warnings(colored_document, colored_real_document['.rm_files'][0]['output_document_position'])
+
+
+@with_remarks(gosper_notebook['.rmn_source'])
+def test_pdf_output():
+    gosper_rmc = fitz.open(f"tests/out/{gosper_notebook['notebook_name']} _remarks.pdf")
+    assert is_valid_pdf(gosper_rmc)
+    assert gosper_rmc.page_count == gosper_notebook["export_properties"]["merged_pages"]
+
+    # There should be a warning, since v3 is not yet supported by the rmc-renderer
+    assert_scrybble_warning_appears_on_page(gosper_rmc, gosper_notebook['.rm_files'][0]['output_document_position'])
+    assert_scrybble_warning_appears_on_page(gosper_rmc, gosper_notebook['.rm_files'][1]['output_document_position'])
+    assert_scrybble_warning_appears_on_page(gosper_rmc, gosper_notebook['.rm_files'][2]['output_document_position'])
 
 
 r"""
@@ -206,7 +206,7 @@ Lessons about parsita.
    parser_that_must_exist_around_it >> parser_that_follows >> another_parser << the_parser_you_care_about >> after_the_parser_you_care_about
    So:
    `{...} >> yes << whatever` => `Success<yes>`
-3. Lambdas are evil. Do not use lambdas to create abstractions.
+3. Lambdas are **evil**. Do not use lambdas to create abstractions.
    While it may seem attractive to write a lambda to express a common pattern, this is not a good idea.
    The operators in parsita have specific meaning, and parsita is a language expressed with operators.
    When you write a function, the result of the operator is lost.
@@ -244,10 +244,8 @@ h1_tag = lit("# ")
 h2_tag = lit("## ")
 h3_tag = lit("### ")
 h4_tag = lit("#### ")
-h6_tag = lit("##### ")
+h5_tag = lit("##### ")
 h6_tag = lit("###### ")
-
-h = lambda n, c: lit(n + " ") >> c
 
 @with_remarks("tests/in/highlighter-test")
 @pytest.mark.markdown
